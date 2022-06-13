e6306b97be658aec0011ac85806349e58cddda36
Lazar Karic <kadzajr@gmail.com>
2022-06-13 23:56:30 +0200

    touchups

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::io;
 
 fn main() {
+    println!("Enter your weight (kg):");
     let mut input = String::new();
 
-    io::stdin().read_line(&mut input);
+    io::stdin().read_line(&mut input).unwrap();
 
-    let mut mars_weight = calculate_weight_on_mars(100.0);
-    mars_weight = mars_weight * 1000.0;
+    let weight: f32 = input.trim().parse().unwrap();
+
+    let mars_weight = calculate_weight_on_mars(weight);
 
     println!("Weight on Mars: {}kg", mars_weight);
 }
